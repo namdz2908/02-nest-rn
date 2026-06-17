bf207f44025619ebd5bcd0bf7bc08e65a908f3cd
Nguyen Hoai Nam <nama1k62cbh@gmail.com>
2026-06-17 21:20:20 +0700

    feat(core): add GetUser decorator and extend user schema with restaurants

--- src/decorators/customize.ts
+++ src/decorators/customize.ts
@@ -1,5 +1,5 @@
 
-import { SetMetadata } from '@nestjs/common';
+import { SetMetadata, createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 // SetMetadata(key, value); // Ta gán key cho SetMetadata và nó chỉ lưu lại value. Thông qua key để lấy ra value, value ở đây chính là metadata muốn gán
@@ -7,3 +7,10 @@ export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 export const RESPONSE_MESSAGE = 'response_message';
 export const ResponseMessage = (message: string) => SetMetadata(RESPONSE_MESSAGE, message);
+
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
