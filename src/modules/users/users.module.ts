15be438c6462fd5efeee47f84878701eff628f57
Nguyen Hoai Nam <nama1k62cbh@gmail.com>
2026-06-24 22:03:56 +0700

    fix: resolve OrderModel dependency injection in UsersModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { Order, OrderSchema } from '@/modules/orders/schemas/order.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [MongooseModule.forFeature([
+    { name: User.name, schema: UserSchema },
+    { name: Order.name, schema: OrderSchema }
+  ])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
